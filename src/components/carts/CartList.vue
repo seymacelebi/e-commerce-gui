738d7ac642c4a7edf058c5825b761d59124eb594
Şeyma Çelebi <seyma.celebi@intimeinfo.com.tr>
2023-05-17 10:32:56 +0300

    cart list ui

--- src/components/carts/CartList.vue
+++ src/components/carts/CartList.vue
@@ -1,9 +1,56 @@
 <template>
   <AppHeader></AppHeader>
 
-  <v-row v-for="(item, index) in getBasketGetters" :key="index">
-    <v-col cols="8" class="mt-15">
-      <v-card color="white" theme="dark" class="ml-15 mt-10" :value="index">
+  <v-row>
+    <v-col
+      cols="8"
+      class="mt-15"
+      v-for="(item, index) in getBasketGetters"
+      :key="index"
+    >
+      <v-card class="fill-height">
+        <v-row dense>
+          <v-col cols="12">
+            <v-card color="white">
+              <v-row>
+                <v-col cols="4">
+                  <v-avatar class="ma-3" size="250" rounded="0">
+                    <v-img
+                      :src="item.images[0]"
+                      class="ml-15 align-center"
+                    ></v-img>
+                  </v-avatar>
+                </v-col>
+                <v-col cols="8">
+                  <v-card-title class="text-h5">
+                    {{ item.title }}
+                  </v-card-title>
+                  <v-card-subtitle> {{ item.description }}</v-card-subtitle>
+
+                  <v-card-actions>
+                    <v-btn
+                      variant="outlined"
+                      size="small"
+                      icon="mdi-plus"
+                      @click="addProduct(item)"
+                    >
+                    </v-btn>
+                    <v-card-title> {{ item.quantity + 1 }} </v-card-title>
+                    <v-btn
+                      variant="outlined"
+                      size="small"
+                      icon="mdi-minus"
+                      @click.prevent="removeProduct(item)"
+                    >
+                    </v-btn>
+                  </v-card-actions>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+      <!-- <v-card color="white" class="ml-15 mt-10" :value="index">
         <div class="d-flex flex-no-wrap justify-space-between">
           <v-avatar class="ma-3" size="125" rounded="0">
             <v-img :src="item.images[0]"></v-img>
@@ -33,27 +80,10 @@
             </v-card-actions>
           </div>
         </div>
-      </v-card>
-      <v-divider></v-divider>
-      <v-row>
-        <v-col>
-          <h3 class="d-flex flex-row-reverse mb-6 mt-4">
-            Sepet Tutarı: {{ totalPrice }}
-          </h3>
-          <div class="d-flex flex-row-reverse mb-3">
-            <v-btn
-              variant="outlined"
-              class="ma-2 pa-2"
-              v-if="getBasketGetters.length > 0"
-            >
-              SATIN AL
-            </v-btn>
-          </div>
-        </v-col>
-      </v-row>
+      </v-card> -->
     </v-col>
-    <v-col cols="4" class="mt-15">
-      <v-card class="mx-auto" max-width="344">
+    <v-col cols="4" class="mt-15 ml-15 top-right">
+      <v-card class="auto ml-15" max-width="344">
         <v-card-item>
           <div>
             <div class="text-h4 mb-1">Checkout</div>
@@ -115,3 +145,15 @@ export default defineComponent({
   components: { AppHeader },
 });
 </script>
+<style scoped>
+.button-container {
+  position: relative;
+}
+
+.top-right {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 10px;
+}
+</style>
