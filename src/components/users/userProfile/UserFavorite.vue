d2ff1fdfee54794c6abadaecd6d2fe6650ee5857
Şeyma Çelebi <seyma.celebi@intimeinfo.com.tr>
2023-05-18 16:50:09 +0300

    ...

--- src/components/users/userProfile/UserFavorite.vue
+++ src/components/users/userProfile/UserFavorite.vue
@@ -1,5 +1,44 @@
 <template>
-  <v-row
+  <v-row>
+    <v-col v-if="getFavoriteProduct.length === 0" class="mt-5">
+      <v-alert density="compact" icon="mdi-alert-circle">
+        Favori ürün bulunamadı.
+      </v-alert></v-col
+    >
+
+    <v-col
+      cols="12"
+      class="mt-15"
+      v-for="(item, index) in getFavoriteProduct"
+      :key="index"
+    >
+      <v-card class="fill-height">
+        <v-row>
+          <v-col cols="12">
+            <v-card color="white">
+              <v-row>
+                <v-col cols="4">
+                  <v-avatar class="ma-3" size="200" rounded="0">
+                    <v-img
+                      :src="item.images[0]"
+                      class="ml-15 align-center"
+                    ></v-img>
+                  </v-avatar>
+                </v-col>
+                <v-col cols="8">
+                  <v-card-title class="text-h5">
+                    {{ item.title }}
+                  </v-card-title>
+                  <v-card-subtitle> {{ item.description }}</v-card-subtitle>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-col>
+  </v-row>
+  <!-- <v-row
     justify="center"
     v-for="(item, index) in getFavoriteProduct"
     :key="index"
@@ -19,7 +58,7 @@
         </div>
       </v-card>
     </v-col>
-  </v-row>
+  </v-row> -->
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
